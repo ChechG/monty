dd54e143eb91675a3e079c6d88ac558ac0f8ee6e
ChechG <2100@holbertonschool.com>
2020-12-22 18:34:57 +0000

    mul

--- function3.c
+++ function3.c
@@ -6,3 +6,16 @@
  * Return: void.
  */
 void f_mul(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp1;
+
+    if (stackt_len(*stack) < 2)
+    {
+        fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		free_doubly(*stack);
+		exit(EXIT_FAILURE);
+    }
+    tmp1 = (*stack)->next;
+    tmp1->n *= (*stack)->n;
+    f_pop(stack, line_number);
+}
